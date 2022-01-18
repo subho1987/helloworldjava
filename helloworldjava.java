f1dfe74414930c214c7741ee7afc84b9286c87bc
subho1987 <47502578+subho1987@users.noreply.github.com>
2022-01-18 16:50:46 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,5 +1,5 @@
 public class helloworldjava {
-   public static void main(String[] args) 
+   public static void main(String[] args) { 
     System.out.println("Hello World from DevOps- success polling test");
     }
 }
